4a4dd8c95822ae36167cc59b9cf8d02f2f071293
Daylan <daylanberry@gmail.com>
2020-01-03 16:47:10 -0800

    updating using reselect

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import  { createSelector } from 'reselect'
+
+//importselector
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (acc, item) =>
+        acc + item.quantity,
+      0
+    )
+)
