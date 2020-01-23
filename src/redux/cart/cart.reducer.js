75efad7d6c59d7a6d8da12ae965654a0f172222d
Daylan <daylanberry@gmail.com>
2020-01-23 13:15:45 -0800

    created checkout

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,7 @@
 import Redux from 'react-redux'
 import CartActionTypes from './cart.types.js'
 
-import { addItemToCart } from './cart.utils.js'
+import { addItemToCart, removeItemFromCart } from './cart.utils.js'
 
 
 const INITIAL_STATE = {
@@ -21,6 +21,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
       }
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+      }
+
+      case CartActionTypes.REMOVE_ITEM:
+        return {
+          ...state,
+          cartItems: removeItemFromCart(state.cartItems, action.payload)
+        }
 
     default:
       return state
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   return [...cartItems, {...cartItemToAdd, quantity: 1} ]
 
 }
+
+export const removeItemFromCart = (cartItems, toRemove) => {
+  var existing = cartItems.find(item =>
+    item.id === toRemove.id
+  )
+
+  if (existing.quantity === 1) {
+    return cartItems.filter(item =>
+      item.id !== toRemove.id)
+  }
+
+  return cartItems.map(item =>
+    item.id === toRemove.id ?
+    {...item, quantity: item.quantity - 1}
+    : item
+  )
+}
