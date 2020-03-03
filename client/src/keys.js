09603a86edd590a2718752dee5304c6aa5a5869e
Daylan <daylanberry@gmail.com>
2020-03-03 13:42:20 -0800

    added chat feature

---
+++ client/src/keys.js
@@ -0,0 +1,6 @@
+const keys = {
+  instanceLocator: 'v1:us1:a647cd76-3e2d-4496-b784-a2ccab39eae7',
+  tokenUrl: 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/a647cd76-3e2d-4496-b784-a2ccab39eae7/token'
+}
+
+export default keys;
