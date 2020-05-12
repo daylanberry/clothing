92bb1c073d2858ea7189b9105736d80650728aea
Daylan <daylanberry@gmail.com>
2020-05-11 21:01:02 -0700

    Updates

---
+++ precache-manifest.361b9ee2d833e2969045a03fc94774c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1d637cd1b1f946bc5385265de89bbb",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "0bf4beb1fb8a1a7505e2",
+    "url": "/monsters/static/css/main.8f3968ab.chunk.css"
+  },
+  {
+    "revision": "1b43d2eaa999d2a50f77",
+    "url": "/monsters/static/js/2.c698f0ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.c698f0ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bf4beb1fb8a1a7505e2",
+    "url": "/monsters/static/js/main.fe85e4fc.chunk.js"
+  },
+  {
+    "revision": "ba67cd212ced8a3c1e1d",
+    "url": "/monsters/static/js/runtime-main.4be90965.js"
+  }
+]);
